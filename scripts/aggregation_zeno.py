5829357ec4112d4a3d5a169ed3d1d0a348005bfa
Zeno Romio <zeno.romio@gmail.com>
2025-06-11 18:44:50 +0200

    added more circles

--- scripts/aggregation_zeno.py
+++ scripts/aggregation_zeno.py
@@ -41,19 +41,15 @@ class State(Enum):
 class CockroachConfig(Config):
     max_speed: float = 2
     obstacle_weight: float = 80
-    alpha: float = 0.4
+    alpha: float = 0.001
     beta: float = 0.6
-    join_time: int = 50
-    leave_time: int = 50
+    join_time: int = 5
+    leave_time: int = 6
     density_threshold: int = 4
 
 
 class CockroachAgent(Agent):
-    aggregate: bool = False
-
     config: CockroachConfig
-
-    state: str = 'wandering'
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -61,28 +57,70 @@ class CockroachAgent(Agent):
         self.join_timer = 0
         self.leave_timer = 0
 
+    def count_sites(self):
+        return self._sites
+
     def change_position(self, dt: float = 1.0):
         self.there_is_no_escape()
 
     def neighbors_in_radius(self):
-        return self.in_proximity_accuracy()
+        return self.in_proximity_performance()
 
     def update(self) -> None:
+        neighbors_in_rad: int = 0
+        for _ in self.neighbors_in_radius():
+            neighbors_in_rad += 1
+        inside_site = self.on_site()
+        sites_exist = True if self.count_sites() else False
 
-        if self.on_site():
+        # wandering
+        if self.state is State.WANDERING:
+            self.pos += self.move
+            if sites_exist and inside_site and random.random() < p_join(neighbors_in_rad, self.config.alpha):
+                self.state = State.JOINING
+                self.join_timer = self.config.join_time
+
+        # joining
+        elif self.state is State.JOINING:
+            if not inside_site:
+                self.state = State.WANDERING
+                return
+            self.pos += self.move
+            self.join_timer -= 1
+            if self.join_timer <= 0:
+                self.state = State.STILL
+
+        # still
+        elif self.state is State.STILL:
+            if not inside_site:
+                self.state = State.WANDERING
+                return
             self.freeze_movement()
-        else:
-            self.pos += self.move * 2
+
+            if random.random() < p_leave(neighbors_in_rad, self.config.beta):
+                self.state = State.LEAVING
+                self.leave_timer = self.config.leave_time
+
+        # leaving
+        elif self.state is State.LEAVING:
+            self.pos += self.move
+            self.leave_timer -= 1
+            if self.leave_timer <= 0:
+                self.state = State.WANDERING
 
 
 (
     Simulation(
         CockroachConfig(
-            image_rotation=True, movement_speed=1, radius=150, seed=1)
+            image_rotation=True, movement_speed=2, radius=150, seed=1)
     )
     .spawn_site(
-        image_path=str(BASE_DIR / "files" / "circle_copy.png"),
-        x=375,
+        image_path=str(BASE_DIR / "files" / "circle_big.png"),
+        x=200,
+        y=375)
+    .spawn_site(
+        image_path=str(BASE_DIR / "files" / "circle.png"),
+        x=575,
         y=375)
     .batch_spawn_agents(
         count=100,
