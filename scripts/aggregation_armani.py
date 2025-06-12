4fd4bfd732fe4672bad890fdf63a8b65c8dcf785
ClozeLine <armanihammer@gmail.com>
2025-06-12 17:23:19 +0200

    misc changes

--- scripts/aggregation_armani.py
+++ scripts/aggregation_armani.py
@@ -27,7 +27,7 @@ def p_join(n, alpha):
 
 
 def p_leave(n, beta):
-    return math.exp(-beta * n)
+    return math.exp(-beta * n**2)
 
 
 class State(Enum):
@@ -41,7 +41,7 @@ class State(Enum):
 class CockroachConfig(Config):
     max_speed: float = 2
     obstacle_weight: float = 80
-    alpha: float = 0.001
+    alpha: float = 0.5
     beta: float = 0.6
     join_time: int = 5
     leave_time: int = 6
@@ -72,17 +72,19 @@ class CockroachAgent(Agent):
             neighbors_in_rad += 1
         inside_site = self.on_site()
         sites_exist = True if self.count_sites() else False
+        site_like = inside_site if sites_exist else (neighbors_in_rad >= self.config.density_threshold)
+        is_on_cluster = inside_site or (not sites_exist and neighbors_in_rad >= self.config.density_threshold)
 
         # wandering
         if self.state is State.WANDERING:
             self.pos += self.move
-            if sites_exist and inside_site and random.random() < p_join(neighbors_in_rad, self.config.alpha):
+            if is_on_cluster and random.random() < p_join(neighbors_in_rad, self.config.alpha):
                 self.state = State.JOINING
                 self.join_timer = self.config.join_time
 
         # joining
         elif self.state is State.JOINING:
-            if not inside_site:
+            if not is_on_cluster:
                 self.state = State.WANDERING
                 return
             self.pos += self.move
@@ -92,7 +94,7 @@ class CockroachAgent(Agent):
 
         # still
         elif self.state is State.STILL:
-            if not inside_site:
+            if not is_on_cluster:
                 self.state = State.WANDERING
                 return
             self.freeze_movement()
@@ -112,11 +114,15 @@ class CockroachAgent(Agent):
 (
     Simulation(
         CockroachConfig(
-            image_rotation=True, movement_speed=2, radius=150, seed=1)
+            image_rotation=True, movement_speed=2, radius=50, seed=1)
     )
     .spawn_site(
         image_path=str(BASE_DIR / "files" / "circle_big.png"),
-        x=375,
+        x=200,
+        y=375)
+    .spawn_site(
+        image_path=str(BASE_DIR / "files" / "circle.png"),
+        x=575,
         y=375)
     .batch_spawn_agents(
         count=100,
