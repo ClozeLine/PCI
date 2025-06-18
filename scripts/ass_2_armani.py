0cfd59be8f2535ad3354abb5a89599f0e3be53bf
ClozeLine <armanihammer@gmail.com>
2025-06-18 14:45:36 +0200

    misc changes

--- scripts/ass_2_armani.py
+++ scripts/ass_2_armani.py
@@ -5,7 +5,6 @@
 # - willingness to breed (rabbits)
 # rabbit can eat poisonous berries, get infected, and contaminate fox (if eaten)
 
-
 from dataclasses import dataclass
 from vi import Agent, Config, Simulation
 from pygame.math import Vector2
@@ -16,6 +15,9 @@ import pygame
 import numpy
 
 precomputed_angles = [random.uniform(0, 2 * numpy.pi) for _ in range(1000)]
+PLANT_COUNTER = 10
+PREY_COUNTER = 100
+PREDATOR_COUNTER = 10
 
 
 def calc_LJ_force(distance, epsilon=100, sigma=0.01):
@@ -26,103 +28,117 @@ def calc_LJ_force(distance, epsilon=100, sigma=0.01):
 
 @dataclass
 class PredatorPreyConfig(Config):
-    prey_speed = 1
+    # general
+    max_food = 100
+    start_food = max_food * 0.7
+
+    # predator config
     predator_speed = 1
+    attack_speed_increase = 1.3
+    predator_food_on_eat = 45
+
+    # prey config
+    prey_speed = 1
+    escape_speed_increase = 1.3
+    prey_food_on_eat = 10
+
+    # plant
+    max_plants = 150
 
 
 class PredatorState(Enum):
     WANDERING = 0
+    CHASING = 1
+
+
+class PreyState(Enum):
+    WANDERING = 0
+    FLEEING = 1
 
 
 class PredatorAgent(Agent):
+    config: PredatorPreyConfig
 
     def __init__(self, images, simulation, pos=None, move=None):
         super().__init__(images, simulation, pos, move)
         self.move = pygame.Vector2(random.uniform(-1, 1), random.uniform(-1, 1))
         self.last_move = self.move
-        self.previous_state = "Wandering"
-        self.state = "Wandering"
+        self.previous_state = PredatorState.WANDERING
+        self.state = PredatorState.WANDERING
         self.speed = self.config.prey_speed
-        self.food = 100
+        self.food = self.config.max_food
 
     def update(self):
+        global PREDATOR_COUNTER, PREY_COUNTER
 
-        if self.id == 1:
-            self.change_image(1)
-            # print(pygame.mouse.get_pos())
-            if self.previous_state != self.state:
-                print(self.state)
-
-        for other in self.in_proximity_accuracy():
-            if type(other[0]) == PreyAgent:
-                if self.pos.distance_to(other[0].pos) <= 10:
-                    # print("plant")
-                    self.food += 75
-                    other[0].kill()
-                    if self.food >= 150:
-                        self.food -= 50
+        for agent in self.in_proximity_accuracy():
+            if isinstance(agent[0], PreyAgent):
+                if self.pos.distance_to(agent[0].pos) <= 10:
+                    self.food += self.config.predator_food_on_eat
+                    agent[0].kill()
+                    PREY_COUNTER -= 1
+                    if self.food > self.config.max_food:
+                        self.food = self.config.max_food
                         self.reproduce()
+                        PREDATOR_COUNTER += 1
 
         if self.food <= 0:
             self.kill()
+            PREDATOR_COUNTER -= 1
 
         self.food -= 0.3
         self.previous_state = self.state
 
+    def calculate_speed(self):
+        factor = (self.food / self.config.max_food) * 100
+        return self.move
+
     def change_position(self):
         self.there_is_no_escape()
 
-        if self.state == "Wandering":
+        if self.state == PredatorState.WANDERING:
 
             self.move += Vector2(random.uniform(-1, 1), random.uniform(-1, 1)) * random.uniform(0, 0.3)
 
             if self.move.length() >= 0.8 and self.move.length() != 0:
                 self.move = self.move.normalize()
 
-        else:
-            print(f"State Error: state is {self.state}")
-
         self.last_move = self.move
-        self.pos += self.move
+        self.pos += self.calculate_speed()
 
 
 class PreyAgent(Agent):
+    config: PredatorPreyConfig
+
     def __init__(self, images, simulation, pos=None, move=None):
         super().__init__(images, simulation, pos, move)
         self.move = pygame.Vector2(random.uniform(-1, 1), random.uniform(-1, 1))
         self.last_move = self.move
-        self.previous_state = "Wandering"
-        self.state = "Wandering"
+        self.previous_state = PreyState.WANDERING
+        self.state = PreyState.WANDERING
         self.speed = self.config.predator_speed
-        self.food = 100
+        self.food = self.config.max_food
 
     def update(self):
+        global PLANT_COUNTER, PREY_COUNTER
 
-        if self.state == "Wandering":
+        if self.state == PreyState.WANDERING:
             pass
 
-
-        else:
-            print(f"State Error: state is {self.state}")
-
-        for other in self.in_proximity_accuracy():
-            if type(other[0]) == PlantAgent:
-                if self.pos.distance_to(other[0].pos) <= 10:
-                    # print("plant")
-                    self.food += 15
-                    if self.food >= 150:
-                        self.food -= 50
+        for agent in self.in_proximity_accuracy():
+            if isinstance(agent[0], PlantAgent):
+                if self.pos.distance_to(agent[0].pos) <= 10:
+                    self.food += self.config.prey_food_on_eat
+                    if self.food > self.config.max_food:
+                        self.food = self.config.max_food
                         self.reproduce()
-                    other[0].kill()
-
-        if self.id == 11:
-            self.change_image(1)
-            # print(pygame.mouse.get_pos())
-            if self.previous_state != self.state:
-                print(self.state)
+                        PREY_COUNTER += 1
+                    agent[0].kill()
+                    PLANT_COUNTER -= 1
 
         if self.food <= 0:
             self.kill()
+            PREY_COUNTER -= 1
 
         self.food -= 0.1
         self.previous_state = self.state
@@ -130,7 +146,7 @@ class PreyAgent(Agent):
     def change_position(self):
         self.there_is_no_escape()
 
-        if self.state == "Wandering":
+        if self.state == PreyState.WANDERING:
 
             self.move += Vector2(random.uniform(-1, 1), random.uniform(-1, 1)) * random.uniform(0, 0.3)
 
@@ -145,16 +161,19 @@ class PreyAgent(Agent):
 
 
 class PlantAgent(Agent):
+    config: PredatorPreyConfig
 
     def __init__(self, images, simulation, pos=None, move=None):
         super().__init__(images, simulation, pos, move)
         self.counter = 0
-        self.maxCount = 200
         self.angle_index = 0
 
     def update(self):
-        if self.counter == self.maxCount:
+        global PLANT_COUNTER
+        print(PLANT_COUNTER)
+        if self.counter == self.config.max_food and PLANT_COUNTER < self.config.max_plants:
             self.reproduce()
+            PLANT_COUNTER += 1
 
             if random.random() < 0.5:
                 self.pos = pygame.math.Vector2(random.randint(0, 750, ), random.randint(0, 750))
@@ -165,7 +184,6 @@ class PlantAgent(Agent):
                 self.pos += 10 * direction
 
             self.counter = 0
-
         self.counter += 1
 
     def change_position(self):
@@ -179,9 +197,9 @@ class PlantAgent(Agent):
 
     )
 
-    .batch_spawn_agents(10, PredatorAgent, images=["../files/Target1.png", "../files/Target6.png"])
-    .batch_spawn_agents(100, PreyAgent, images=["../files/triangle.png", "../files/Target5.png"])
-    .batch_spawn_agents(100, PlantAgent, images=["../files/plant.png"])
+    .batch_spawn_agents(PREDATOR_COUNTER, PredatorAgent, images=["../files/Target1.png", "../files/Target6.png"])
+    .batch_spawn_agents(PREY_COUNTER, PreyAgent, images=["../files/triangle.png", "../files/Target5.png"])
+    .batch_spawn_agents(PLANT_COUNTER, PlantAgent, images=["../files/plant.png"])
     .run()
 )
 
